61eabef1ec1ff188e7085dc51cf1a72bcf05c9b0
Kyle Morton <kylemortondevelopment@gmail.com>
2020-03-02 23:52:17 -0600

    added base.html, before starting form

--- riotactproject/boardsapp/views.py
+++ riotactproject/boardsapp/views.py
@@ -1,10 +1,13 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from boardsapp.models import Board
 
 # Create your views here.
-from boardsapp.models import Board
 
 
 def home(request):
     boards = Board.objects.all()
     return render(request, 'home.html', { 'boards': boards })
+
+def board_topics(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+    return render(request, 'topics.html', {'board': board})
